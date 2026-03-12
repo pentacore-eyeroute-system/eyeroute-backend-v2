0da29f7b51e610fdc8a27aba4985d22fb3b571fb
finalFactorial <ragm923@gmail.com>
2026-03-12 09:41:35 +0800

    fix: change column name from ntf to ntt

--- database/seeders/notificationTypesSeeder.js
+++ database/seeders/notificationTypesSeeder.js
@@ -4,16 +4,16 @@ async function seedNotificationTypes() {
     try {
         await NotificationType.bulkCreate([
             {
-                ntf_title: 'Low Battery Warning',
-                ntf_description: 'The device battery is running low. Please charge your device to avoid interruptions.'
+                ntt_title: 'Low Battery Warning',
+                ntt_description: 'The device battery is running low. Please charge your device to avoid interruptions.'
             },
             {
-                ntf_title: 'Device Connected',
-                ntf_description: 'The device is now online. All functions are available.'
+                ntt_title: 'Device Connected',
+                ntt_description: 'The device is now online. All functions are available.'
             },
             {
-                ntf_title: 'Device Disconnected',
-                ntf_description: 'The device is currently offline. Some functions may be unavailable until the connection is restored.'
+                ntt_title: 'Device Disconnected',
+                ntt_description: 'The device is currently offline. Some functions may be unavailable until the connection is restored.'
             },
         ]);
 
