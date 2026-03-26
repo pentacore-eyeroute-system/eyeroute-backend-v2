8a1930c27eca6477465dd1b59fcee04502cdf0c9
finalFactorial <ragm923@gmail.com>
2026-03-26 17:00:16 +0800

    feat: - add websocket server - setup server connection to location websocket - create POST route to store and broadcast latest coordinates to db and location websocket respectively

--- src/routes/locationRoutes.js
+++ src/routes/locationRoutes.js
@@ -5,7 +5,10 @@ import { LocationController } from '../controllers/locationController.js';
 const router = express.Router();
 const locationController = new LocationController();
 
+// POST route
+router.post('/push-latest-location/:serialNumber', authenticateCognitoToken, locationController.pushLatestLocation); // serial number points to iot serial number
+
 // GET route
-router.get('/latest-location/:id', authenticateCognitoToken, locationController.getLatestLocation); // id points to pvi id
+router.get('/get-latest-location/:id', authenticateCognitoToken, locationController.getLatestLocation); // id points to pvi id
 
 export default router;
--- src/services/locationService.js
+++ src/services/locationService.js
@@ -1,6 +1,15 @@
 import { Location } from "../models/locationModel.js";
 
 export class LocationService {
+    async pushLatestLocation(activeWearableId, iotLatestCoordinates) {
+        await Location.create({
+            loc_linked_active_wearable_id : activeWearableId,
+            loc_latitude : iotLatestCoordinates.latitude,
+            loc_longitude : iotLatestCoordinates.longitude,
+            loc_recorded_at : iotLatestCoordinates.timestamp, 
+        });
+    };
+
     async getLatestLocation(activeWearableId) {
         const location = await Location.findOne({ 
             where : { loc_linked_active_wearable_id : activeWearableId },
